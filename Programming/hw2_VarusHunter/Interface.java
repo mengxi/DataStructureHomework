b7ab177e63074e82132fcee4a7dc8972ac4b6b0d
jizhu <zinjiggle@gmail.com>
2013-11-10 13:28:02 -0500

    Interface.java

--- Programming/hw2_VarusHunter/Interface.java
+++ Programming/hw2_VarusHunter/Interface.java
@@ -1,1 +1,23 @@
-a little
+package hw2_VarusHunter;
+
+import java.lang.String;
+
+
+class Option{
+  int key;
+  String notice;
+  Callable<null> func;
+  Option(int key, String str, Callable<null> func){
+    this.key = key;
+    this.str = str;
+    this.func = func;
+  }
+}
+
+public class Interface{
+  
+
+  public void __init__(){
+
+  }
+}
